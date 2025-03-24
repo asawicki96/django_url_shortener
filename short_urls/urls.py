598c35f0589f0622af7f2550e38244ec262c8cc7
adamsawicki <adam.sawicki@alvisoft.pl>
2025-03-24 13:02:15 +0100

    Changed url for retrieving short_urls

--- short_urls/urls.py
+++ short_urls/urls.py
@@ -4,5 +4,5 @@ app_name = "short_urls"
 
 urlpatterns = [
     path("", CreateShortURLView.as_view(), name="create_short_url"),
-    path("<str:domain>/<str:alias>", RetrieveShortURLView.as_view(), name="retrieve_short_url")
+    path("<str:alias>/", RetrieveShortURLView.as_view(), name="retrieve_short_url")
 ]
--- tests/short_urls/test_views.py
+++ tests/short_urls/test_views.py
@@ -36,13 +36,13 @@ class TestCreateShortURLView:
 
 
 class TestUrlRetrieveShortURLView:
-    def get_view_url(self, domain: str, alias: str) -> str:
-        return reverse("short_urls:retrieve_short_url", kwargs={"domain": domain, "alias": alias})
+    def get_view_url(self, alias: str) -> str:
+        return reverse("short_urls:retrieve_short_url", kwargs={"alias": alias})
     
-    def test_view_response_data(self, client: APIClient, example_url: str, settings):
+    def test_view_response_data(self, client: APIClient, example_url: str):
         existing_short_url = ShortURL.objects.create(long_url=example_url, alias="xyz")
         
-        response = client.get(path=self.get_view_url(domain=settings.DOMAIN_NAME, alias=existing_short_url.alias))
+        response = client.get(path=self.get_view_url(alias=existing_short_url.alias))
 
         assert response.status_code == status.HTTP_200_OK
         assert response.data == {
